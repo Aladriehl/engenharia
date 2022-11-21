5104a8e3ed639bb23c8800f3d80beb5d75c3b71b
Vinicius <matias.uplink@outlook.com>
2022-11-21 19:08:28 -0300

    Adicionando Class Vinicius

---
+++ src/main/java/Vinicius.java
@@ -0,0 +1,8 @@
+public class Vinicius {
+    private int atributo1,atributo2,atributo3;
+
+    public void soma(int atributo1, int atributo2){
+        this.atributo3=atributo1+atributo2;
+        System.out.println("Resultado da soma de: "+ atributo1+" + "+atributo2+" = "+atributo3);
+    }
+}
