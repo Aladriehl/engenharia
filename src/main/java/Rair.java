3b8976530cbe583eb24b6e2aa4fe84e0d1944e1a
Emanoel Rair Amâncio Lima <rair88@gmail.com>
2022-11-21 18:44:57 -0300

    Classe com os 3 atributos:

---
+++ src/main/java/Rair.java
@@ -0,0 +1,9 @@
+public class Rair {
+    private int atributo1,atributo2,atributo3;
+
+    public Rair(int atributo1, int atributo2){
+        this.atributo3=atributo1+atributo2;
+        System.out.println("Resultado da soma de: "+ atributo1+" + "+atributo2+" = "+atributo3);
+    }
+
+}
